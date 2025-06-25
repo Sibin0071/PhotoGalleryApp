20ca1a0e94e3131d2790ea8935218a334b6d64f9
Sibin Steephan <Sibin.Steephan@buck.com>
2025-06-25 12:42:40 -0400

    fixxxxxxxx

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const imageModal = document.getElementById('imageModal');
+if (imageModal) {
+    imageModal.addEventListener('show.bs.modal', function (event) {
+        const trigger = event.relatedTarget;
+        const imgUrl = trigger.getAttribute('data-img-url');
+        const modalImage = imageModal.querySelector('#modalImage');
+        modalImage.src = imgUrl;
+    });
+}
+
