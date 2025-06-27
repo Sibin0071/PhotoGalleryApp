0a5036238d4bd4cbcba576b6edb4ba6b9c0e498d
Sibin Steephan <Sibin.Steephan@buck.com>
2025-06-27 10:06:07 -0400

    Testing...

--- obj/Debug/net8.0/PhotoGalleryApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhotoGalleryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoGalleryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6749e199b15dde1afc59908fc889b1fb2996321c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a37e30fd59b6cf598b14c310b5b7e8c0c5befe51")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoGalleryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoGalleryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
